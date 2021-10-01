574c659b5ace0bc8547c2eded190f102c6caf127
John Kerl <kerl.john.r@gmail.com>
2021-10-01 11:46:18 -0400

    Fuller on-host toolkit

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ Simplification of https://github.com/johnkerl/scripts/tree/main/suitcase
 
 ## Usage
 
-Using `scp`/`rsync`/etc put this directory into `~/away` on the remote host.
-
-SSH there and `. ~/away/rc`.
+* Raw checkout of this repo has several good bits.
+* Optionally run `sh populate.sh` to put more good bits into `./bin-general`.
+* Using `scp`/`rsync`/etc put this directory into `~/away` on the remote host.
+  * `rsync -rtlzv --delete $(pwd)/ remote-host-name.goes.here:away/`
+  * Pay special attention to both trailing slashes -- they're important
+* SSH there and `. ~/away/rc`.
---
+++ populate.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# Items from github.com/johnkerl/scripts
+
+fundam=$HOME/scripts/fundam
+
+for x in \
+  ax \
+  backn \
+  base64dec \
+  base64enc \
+  base64tohex \
+  bffind \
+  bu \
+  catnz \
+  catwhich \
+  chop \
+  clot \
+  colprint \
+  cpick \
+  crafter \
+  craftern \
+  cratchange \
+  crbefore \
+  creach \
+  d2b \
+  d2h \
+  depthsort \
+  dspc \
+  each \
+  escurl \
+  fage \
+  fminus \
+  gmsort \
+  gmt2sec \
+  grepr \
+  h2b \
+  h2d \
+  hex \
+  interleave \
+  Jj \
+  justtime \
+  keep-lines-after \
+  keep-lines-before \
+  keep-lines-between \
+  krepl \
+  left \
+  lensort \
+  linepart \
+  lines \
+  linesort \
+  lright \
+  lrparen \
+  lsr \
+  mdate \
+  mgrep \
+  mgrepv \
+  mrepl \
+  mwhich \
+  namecat \
+  nbu \
+  nf \
+  nocolon \
+  parapart \
+  parlensort \
+  parside \
+  parsort \
+  path-whereis \
+  peach \
+  pjoin \
+  printthrice \
+  printthricej \
+  printtwice \
+  printtwicej \
+  right \
+  rmr \
+  scrunch \
+  sec2gmt \
+  snurc \
+  stremp \
+  swipe \
+  systime \
+  uind \
+  unhex \
+  uniqm \
+  upcase \
+  ustamp
+do
+  cp $fundam/$x bin-general/
+done
