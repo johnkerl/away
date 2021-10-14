cd4ae7cf71798aaf02d9f5f8fe48f6e572936490
John Kerl <kerl.john.r@gmail.com>
2021-10-14 14:23:01 -0400

    neaten populate.sh

--- populate.sh
+++ populate.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 cp ~/scripts/fundam/* ./bin-general
-cp -r ~/bin/site/*    ./bin-project
 
 # cd ~/git/johnkerl/miller
 # env GOOS=linux GOARCH=x86_64 go build mlr
 # cp the binary to ./bin-general
+if [ -f mlr.lnx   ]; then cp mlr.lnx   bin-general/mlr  ; fi
+if [ -f lumin.lnx ]; then cp lumin.lnx bin-general/lumin; fi
 
 cd bin-general
 ln -f -s /usr/local/bin/python3 python
